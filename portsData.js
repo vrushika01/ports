5e2c9ca3186f12223766b08d902f8453e923af88
vrushika patel <98327042+vrushika01@users.noreply.github.com>
2025-01-29 12:20:25 +0530

    Add files via upload

---
+++ portsData.js
@@ -0,0 +1,335 @@
+const portsData = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Mandvi Port group",
+                "info": "Mandvi, a fair weather lighterage port , is situated on the right bank of the river Rukmavati in the Gulf of kutch on the west coast of India.",
+                "detailsPage":"mandvi.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [69.7211, 22.7766]
+            },
+            "ports": [
+                {
+                    "name": "Mundra (MPSEZ)",
+                    "coordinates": [69.721329, 22.839520],
+                    "detailsPage": "Mundra (MPSEZ).html"
+                },
+                {
+                    "name": "Jakhau",
+                    "coordinates": [68.6079, 23.2424],
+                    "detailsPage": "Jakhau.html"
+                }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Navlakhi Port group",
+                "info": "Navlakhi is a all weather lighterage port situated on the south east end of Gulf of Kutch, on the wester coast of India.",
+                "detailsPage":"navlakhi.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [70.4529,22.9574]
+            },
+            "ports": [
+                {
+                    "name": "GMB Jetty",
+                    "info": "GMB jetty info",
+                    "coordinates": [70.45, 22.966667],
+                    "detailsPage": "GMB Jetty.html"
+                }
+                //,
+                // {
+                //     "name": "Jaydeep",
+                //     "coordinates": [ , ],
+                //     "detailsPage": "Jaydeep.html"
+                // },
+                // {
+                //     "name": "Chaugule",
+                //     "coordinates": [ , ],
+                //     "detailsPage": "Chaugule.html"
+                // },
+                // {
+                //     "name": "United Shippers ltd.",
+                //     "coordinates": [ , ],
+                //     "detailsPage": "United Shippers ltd.html"
+                // },
+                // {
+                //     "name": "Shriji Shipping",
+                //     "coordinates": [ , ],
+                //     "detailsPage": "Shriji Shipping.html"
+                // }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Porbandar Port group",
+                "info": "Porbandar is an all weather port with direct berthing facilities. It is situated on the west coast of Saurashtra, facing the Arabian sea.",
+                "detailsPage":"porbandar.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [69.5887, 21.6241]
+            },
+            // "ports": [
+            //     {
+            //         "name": "GMB Jetty",
+            //         "coordinates": [, ],
+            //         "detailsPage": "P.html"
+            //     },
+            //     {
+            //         "name": "SAURASTRA Cement Co.",
+            //         "coordinates": [, ],
+            //         "detailsPage": "SAURASTRA.html"
+            //     }
+            // ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Veraval Port group",
+                "info": "Veraval, a fair weather lighterage port is located in the South west coast of Saurashtra on the west coast of India.",
+                "detailsPage":"veraval.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [70.3837, 20.9061]
+            },
+            "ports": [
+                {
+                    "name": "Mul-Dwarka",
+                    "coordinates": [ 70.65653 ,20.75702 ],
+                    "detailsPage": "Mul-Dwarka.html"
+                },
+                {
+                    "name": "Navabandar",
+                    "coordinates": [71.08, 20.74 ],
+                    "detailsPage": "Navabandar.html"
+                },
+                // {
+                //     "name": "Kotda",
+                //     "coordinates": [80.326744, 13.252851 ],
+                //     "detailsPage": "kotda.html"
+                // },
+                // {
+                //     "name": "Madhvad",
+                //     "coordinates": [,],
+                //     "detailsPage": "madhvad.html"
+                // },
+                {
+                    "name": "Mangrol",
+                    "coordinates": [70.12, 21.12 ],
+                    "detailsPage": "mangrol.html"
+                },
+                {
+                    "name": "Chhara",
+                    "coordinates": [70.737544, 20.72168],
+                    "detailsPage": "chhara.html"
+                }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Jafrabad Port group",
+                "info": "Jafrabad is an all weather direct berthing port located inside Jafrabad creek in the Gulf of Cambay on the south coast of Saurashtra.",
+                "detailsPage":"jafrabad.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [71.3866, 20.8629]
+            },
+            "ports": [
+                {
+                    "name": "Ultratech Jetty(KOVAYA)",
+                    "coordinates": [ 71.3721668 , 20.8682842],
+                    "detailsPage": "ultratech.html"
+                },
+                {
+                    "name": "Gujrat Pipavav Port Ltd",
+                    "coordinates": [71.4965142, 20.922079],
+                    "detailsPage": "Gujrat Pipavav Port Ltd.html"
+                }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Bhavnagar Port group",
+                "info": "Bhavnagar is an all weather berthing port for smaller vessels located in the Gulf of Cambay on the west coast of India.",
+                "detailsPage":"bhavnagar.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [72.2308, 21.7669]
+            },
+            "ports": [
+                {
+                    "name": "Ghogha",
+                    "coordinates": [72.282922, 21.677171],
+                    "detailsPage": "Ghogha.html"
+                }
+                //,
+                // {
+                //     "name": "Talaja",
+                //     "coordinates": [, ],
+                //     "detailsPage": "talaja.html"
+                // }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Magdalla Port group",
+                "info": "Magdalla is a lighterage port located on the west coast of India in southern Gujarat. It is located on the bank of river Tapi.",
+                "detailsPage":"magdalla.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [72.7311, 21.14295]
+            },
+            "ports": [
+                // {
+                //     "name": "Magdalla port ",
+                //     "coordinates": [, ],
+                //     "detailsPage": "Magdalla port.html"
+                // },
+                {
+                    "name": "HAZIRA ",
+                    "coordinates": [72.6443, 21.0875],
+                    "detailsPage": "HAZIRA.html"
+                },
+                // {
+                //     "name": "Umargaon",
+                //     "coordinates": [, ],
+                //     "detailsPage": "Umargaon.html"
+                // },
+                {
+                    "name": "Maroli",
+                    "coordinates": [72.8899466, 21.0243434],
+                    "detailsPage": "Maroli.html"
+                },
+                {
+                    "name": "Valsad",
+                    "coordinates": [72.750, 20.233],
+                    "detailsPage": "valsad.html"
+                },
+                {
+                    "name": "Bilimora",
+                    "coordinates": [72.9539, 20.7669],
+                    "detailsPage": "Bilimora.html"
+                },
+                // {
+                //     "name": "Onjal ",
+                //     "coordinates": [, ],
+                //     "detailsPage": "Onjal .html"
+                // },
+                {
+                    "name": "Kolak",
+                    "coordinates": [72.883, 20.483],
+                    "detailsPage": "Kolak.html"
+                },
+                // {
+                //     "name": "Umarsadi",
+                //     "coordinates": [, ],
+                //     "detailsPage": "Umarsadi.html"
+                // },
+                {
+                    "name": "Vansi-Borsi",
+                    "coordinates": [ 72.7705,20.9289 ],
+                    "detailsPage": "Vansi-Borsi.html"
+                },
+                // {
+                //     "name": "Nargol",
+                //     "coordinates": [, ],
+                //     "detailsPage": "Nargol.html"
+                // }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Okha Port group",
+                "info": "Okha is an all weather port with direct berthing facilities located on the north-west coast of Saurashtra Peninsula.",
+                "detailsPage":"okha.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [69.0833, 22.4667]
+            },
+            "ports": [
+                {
+                    "name": "Bhogat",
+                    "coordinates": [69.24156, 21.9924159],
+                    "detailsPage": "bhogat.html"
+                },
+                {
+                    "name": "Rupen",
+                    "coordinates": [68.95417, 22.25944],
+                    "detailsPage": "rupen.html"
+                },
+                {
+                    "name": "Beyt",
+                    "coordinates": [69.0928497, 22.4521255],
+                    "detailsPage": "beyt.html"
+                }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Bharuch Port group",
+                "info": "Dahej is a fair weather lighterage port located at the junction of Guljaria and Ban Creek, on the west coast of the country, at Gulf of Cambay.",
+                "detailsPage":"bharuch.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [72.5843, 21.7132]
+            },"ports": [
+                {
+                    "name": "RDMT Jetty",
+                    "coordinates": [72.56089, 21.65976],
+                    "detailsPage": "RDMT Jetty.html"
+                },
+                // {
+                //     "name": "DHIL Jetty",
+                //     "coordinates": [72.315, 21.42 ],
+                //     "detailsPage": "DHIL Jetty.html"
+                // },
+                {
+                    "name": "Godrej Jetty",
+                    "coordinates": [72.9404, 19.0973],
+                    "detailsPage": "Godrej Jetty.html"
+                },
+                // {
+                //     "name": "ISGECT Jetty",
+                //     "coordinates": [, ],
+                //     "detailsPage": "ISGECT Jetty.html"
+                // },
+                {
+                    "name": "GCPTCL(Dahej)",
+                    "coordinates": [72.53790, 21.69255],
+                    "detailsPage": "GCPTCL(Dahej).html"
+                },
+                {
+                    "name": "Petronet LNG (PLL)",
+                    "coordinates": [72.536, 21.6746],
+                    "detailsPage": "Petronet LNG (PLL).html"
+                }
+                // ,
+                // {
+                //     "name": "Adani Petronet Pvt Ltd.",
+                //     "coordinates": [, ],
+                //     "detailsPage": "Adani Petronet Pvt Ltd.html"
+                // }
+                
+            ]
+        }
+    ]
+};
---
+++ script1.js
@@ -0,0 +1,234 @@
+// pop window
+// Initialize the map and set the view to Gujarat
+const map = L.map("map").setView([22.2587, 71.1924], 7);
+
+// Add a tile layer (map appearance)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+// Variable to keep track of the currently highlighted markers
+let highlightedMarkers = [];
+
+// Function to clear highlighted markers
+function clearHighlightedMarkers() {
+  highlightedMarkers.forEach((marker) => map.removeLayer(marker));
+  highlightedMarkers = [];
+}
+
+// Function to show popup with port details
+function showPopup(portName, portInfo, detailsPage) {
+  const popup = document.getElementById("popup");
+  const popupContent = document.getElementById("popupContent");
+
+  popupContent.innerHTML = `
+    <p><strong>Port Name:</strong> ${portName}</p>
+    <p><strong>Details:</strong> ${portInfo}</p>
+    <a href="${detailsPage}" target="_blank" style="display:inline-block; margin-top:10px; text-decoration:none; color:blue;">
+  Know More
+</a>
+
+  `;      
+  popup.style.display = "block";
+}
+
+// Function to close popup
+function closePopup() {
+  const popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
+
+// Function to add individual port markers with a different color
+function addIndividualPortMarkers(ports) {
+    ports.forEach((port) => {
+      const marker = L.marker([port.coordinates[1], port.coordinates[0]], {
+        icon: L.icon({
+          iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png", // Marker icon
+          iconSize: [25, 41],
+          iconAnchor: [12, 41],
+          // Custom color for individual port markers
+          className: "individual-port-marker", // Added custom class for styling
+        }),
+      }).addTo(map);
+      highlightedMarkers.push(marker);
+  
+      // Add tooltip to individual port markers
+      marker.bindTooltip(port.name, {
+        permanent: false,
+        direction: "top",
+        offset: [0, -40],
+      })
+       // Add click event to show popup when an individual port is clicked
+       marker.on("click", function () {
+        showPopup(port.name, port.info, port.detailsPage);
+    });
+});
+}
+
+// Add ports data to the map
+L.geoJSON(portsData, {
+  onEachFeature: function (feature, layer) {
+    const { name, info,detailsPage } = feature.properties;
+
+    layer.on("click", function () {
+      const coordinates = feature.geometry.coordinates;
+
+      // Fly to the clicked port's location
+      if (coordinates) {
+        map.flyTo([coordinates[1], coordinates[0]], 10, { duration: 1.5 });
+      }
+
+//If the feature is a group, display individual ports
+if (feature.ports && Array.isArray(feature.ports)) {
+    clearHighlightedMarkers(); // Clear any previous highlights
+    addIndividualPortMarkers(feature.ports); // Add individual port markers
+  }
+
+      // Show the popup with port details
+      showPopup(name, info, detailsPage);
+    });
+
+    // Show label on hover
+    layer.bindTooltip(name, {
+      permanent: false, // Tooltip appears only on hover
+      direction: "top", // Position the label above the marker
+      offset: [0, -40], // Move label slightly above the marker
+      opacity: 1.0, // Slight transparency for better visibility
+    });
+  },
+
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, {
+      icon: L.icon({
+        iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png", // Marker icon
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+      }),
+    });
+  },
+}).addTo(map);
+
+// Add a search control for ports
+const searchControl = L.control({ position: "topright" });
+
+searchControl.onAdd = function () {
+  const div = L.DomUtil.create("div", "search-container");
+  div.innerHTML = `
+    <input type="text" id="searchBox" placeholder="Search Ports or Groups..." style="width: 200px; padding: 5px; border: 1px solid #ccc; border-radius: 5px;">
+    <ul id="searchResults" style="list-style-type: none; margin: 5px 0 0; padding: 0; border: 1px solid #ccc; border-radius: 5px; 
+        max-height: 150px; overflow-y: auto; background: white; display: none;">
+    </ul>
+  `;
+  return div;
+};
+
+searchControl.addTo(map);
+
+// Debounce function to optimize search
+let debounceTimer;
+function debounce(callback, delay) {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(callback, delay);
+}
+
+// Add search functionality
+document.getElementById("searchBox").addEventListener("input", function (e) {
+  debounce(() => {
+    const searchTerm = e.target.value.toLowerCase();
+    const resultsList = document.getElementById("searchResults");
+    resultsList.innerHTML = "";
+    resultsList.style.display = searchTerm ? "block" : "none";
+
+    clearHighlightedMarkers();
+
+    const matchingResults = [];
+    portsData.features.forEach((feature) => {
+      const { name } = feature.properties;
+
+      if (name.toLowerCase().includes(searchTerm)) {
+        matchingResults.push({
+          name,
+          coordinates: feature.geometry.coordinates,
+          info: feature.properties.info,
+          isGroup: true,
+          group: feature,
+        });
+      }
+
+      if (feature.ports && Array.isArray(feature.ports)) {
+        feature.ports.forEach((port) => {
+          if (port.name.toLowerCase().includes(searchTerm)) {
+            matchingResults.push({
+              name: port.name,
+              coordinates: port.coordinates,
+              info: port.info,
+              isGroup: false,
+            });
+          }
+        });
+      }
+    });
+
+    if (matchingResults.length > 0) {
+      matchingResults.forEach((result) => {
+        const listItem = document.createElement("li");
+        listItem.style.padding = "5px";
+        listItem.style.cursor = "pointer";
+        listItem.style.borderBottom = "1px solid #ccc";
+        listItem.textContent = result.name;
+
+        listItem.addEventListener("click", function () {
+          map.flyTo([result.coordinates[1], result.coordinates[0]], result.isGroup ? 10 : 12, { duration: 1.5 });
+
+          if (result.isGroup) {
+            const groupMarker = L.marker([result.coordinates[1], result.coordinates[0]], {
+              icon: L.divIcon({
+                className: "highlighted-group",
+                html: `<div style="background-color: blue; width: 14px; height: 14px; border-radius: 50%;"> </div>`,
+                iconSize: [14, 14],
+                iconAnchor: [7, 7],
+              }),
+            }).addTo(map);
+            highlightedMarkers.push(groupMarker);
+
+            result.group.ports.forEach((port) => {
+              const portMarker = L.marker([port.coordinates[1], port.coordinates[0]], {
+                icon: L.icon({
+                  iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png", // Marker icon
+                  iconSize: [25, 41],
+                  iconAnchor: [12, 41],
+                  className: "individual-port-marker",
+                }),
+              }).addTo(map);
+              highlightedMarkers.push(portMarker);
+            });
+          } else {
+            const portMarker = L.marker([result.coordinates[1], result.coordinates[0]], {
+              icon: L.icon({
+                iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png", // Marker icon
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                className: "individual-port-marker",
+              }),
+            }).addTo(map);
+            highlightedMarkers.push(portMarker);
+          }
+
+          // Show popup for the selected port
+          showPopup(result.name, result.info);
+
+          resultsList.style.display = "none";
+        });
+
+        resultsList.appendChild(listItem);
+      });
+    } else {
+      const noResultsItem = document.createElement("li");
+      noResultsItem.style.padding = "5px";
+      noResultsItem.style.color = "#999";
+      noResultsItem.textContent = "No matching ports found.";
+      resultsList.appendChild(noResultsItem);
+    }
+  }, 100);
+});
